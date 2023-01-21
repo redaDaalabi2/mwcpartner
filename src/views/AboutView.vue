24056ac21fb376ea4de6dca657ac3b6f7b8300a5
Redadaalabi2001 <daalabir@gmail.com>
2023-01-21 18:37:31 +0100

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div data-aos="fade-up" class="about">
+    <h1 class="font-bold text-lg">This is an about page</h1>
   </div>
 </template>
